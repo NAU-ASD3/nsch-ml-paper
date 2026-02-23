1cbeef018ecd27aa948fa00c2ee8e95b12c16026
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2026-02-23 16:04:15 -0500

    nsch::

--- NSCH_data.R
+++ NSCH_data.R
@@ -3,6 +3,6 @@ remotes::install_github("NAU-ASD3/nsch@get_years_csv")
 if(FALSE){
   unlink("NSCH_data", recursive=TRUE)
 }
-(size_dt <- get_years_csv("NSCH_data", verbose=TRUE))
+(size_dt <- nsch::get_years_csv("NSCH_data", verbose=TRUE))
 dcast(size_dt, year ~ data_type, value.var=c("rows", "cols"))
 
